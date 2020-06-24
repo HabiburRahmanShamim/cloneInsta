48c411cd0a9fc51a66e7863c3c2d0590236604d7
HabiburRahmanShamim <shamimmahmed04@gmail.com>
2020-06-24 21:51:04 +0600

    added user profile frontend 60%

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,33 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+    <div class="row">
+        <div class="col-3 pt-3 pl-5 pr-5 pb-5">
+            <img src="https://th.bing.com/th/id/OIP.TrqiHn-8hoz1tohyRys4PQAAAA?pid=Api&rs=1" style="height: 150px;" class="rounded-circle">
         </div>
+        <div class="col-9 pt-3 pl-4">
+            <div><h2>freecodecamp</h2></div>
+            <div class="d-flex pt-2">
+                <div class="pr-5"><strong>304</strong> posts</div>
+                <div class="pr-5"><strong>50.9k</strong> followers</div>
+                <div class="pr-5"><strong>261</strong> following</div>
+            </div>
+            <div class="pt-2"><b>freeCodeCamp.org</b></div>
+            <div>We're a global community of millions of people learning to code together.
+                    We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div><a href="www.freecodecamp.org"><b>www.freecodecamp.org</b></a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+            <div class="col-4" >
+                <img src="https://instagram.fdac23-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.107.924.924a/s640x640/101418871_149220720095600_2797518877581952974_n.jpg?_nc_ht=instagram.fdac23-1.fna.fbcdn.net&amp;_nc_cat=108&amp;_nc_ohc=JlIM9q_nBgoAX_jw1ur&amp;oh=cbf73eef2032f4da8d6f633d08c89167&amp;oe=5F1B9A4A" class="w-100">
+            </div>
+            <div class="col-4">
+                <img src="https://instagram.fdac23-1.fna.fbcdn.net/v/t51.2885-15/e35/c64.0.548.548a/100898919_639147106680041_7770796487488702501_n.jpg?_nc_ht=instagram.fdac23-1.fna.fbcdn.net&amp;_nc_cat=105&amp;_nc_ohc=1T9oPYhgzXUAX8NSflI&amp;oh=9d628323387b2222a44bf02fb5a0fc34&amp;oe=5F1CE905" class="w-100">
+            </div>
+            <div class="col-4">
+                <img src="https://instagram.fdac23-1.fna.fbcdn.net/v/t51.2885-15/e35/c127.0.495.495a/100950429_172849147497410_8469189274443807895_n.jpg?_nc_ht=instagram.fdac23-1.fna.fbcdn.net&amp;_nc_cat=102&amp;_nc_ohc=_f-WGoHAW8sAX_vf21M&amp;oh=5d113f69ab7b56fa35a4279175a1f17a&amp;oe=5F1CBD84" class="w-100">
+            </div>
     </div>
 </div>
 @endsection
