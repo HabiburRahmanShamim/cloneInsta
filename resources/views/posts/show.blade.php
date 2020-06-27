68902efbed639d96500910e76676bc05f3d085b7
HabiburRahmanShamim <shamimmahmed04@gmail.com>
2020-06-27 14:34:38 +0600

    done show post 60%

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,9 +6,30 @@
             <div class="col-6">
                 <img src="/storage/{{  $post-> image }}" class="w-100">
             </div>
-            <div class="col-5">
-                <h1>{{  $post->user->username }}</h1>
-                <p>{{  $post->caption }}</p>
+            <div class="col-4">
+                <div>
+                    <div class="d-flex align-items-center">
+                        <div class="col-2 pr-5" style="background: white">
+                            <img src="/storage/{{  $post->user->profile->image }}" style="max-width: 40px;" class="rounded-circle">
+                        </div>
+                        <div>
+                           <a href="/profile/{{  $post->user->id }}">
+                               <span class=" pl-2 font-weight-bold text-dark">
+                                   {{  $post->user->username }}
+                               </span>
+                           </a>
+                        </div>
+                    </div>
+                    <hr>
+                    <div>
+                        <a href="/profile/{{  $post->user->id }}">
+                            <span class="font-weight-bold text-dark">
+                                {{  $post->user->username }}
+                            </span>
+                        </a>
+                        <span> {{  $post->caption }}</span>
+                    </div>
+                </div>
 
             </div>
         </div>
